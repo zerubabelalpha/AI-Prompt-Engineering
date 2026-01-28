fa3e246d83b9bcbc3b8cc1e3e392f7bdb59aae1b
zerubabelalpha <https://github.com/zerubabelalpha>
2026-01-28 17:03:45 +0300

    google gemini api model test implementation

---
+++ 01_basic_api.py
@@ -0,0 +1,50 @@
+import google.generativeai as genai
+import os
+from dotenv import load_dotenv
+
+
+#load environment variable
+load_dotenv()
+
+#gonfigure the api
+def setup_gemini():
+    api_key=os.getenv("GOOGLE_API_KEY")
+    if not api_key:
+        print("please set api key in .env")
+        print("get the key from the api")
+        return
+    genai.configure(api_key=api_key)
+    print("api key configured succussfully")
+    return genai
+
+def simple_chat(prompt, model_name="gemini-2.5-flash"):
+    try:
+        model=genai.GenerativeModel(model_name)
+
+        response=model.generate_content(prompt)
+
+        print(f"You asked {prompt}")
+        print(f"{'*'*4}")
+        print(response.text)
+        return response
+    except Exception as e:
+        print("error occured",e)
+        return 
+    
+if __name__=="__main__":
+    print("====  google api testing  ====")
+
+    genai=setup_gemini()
+    if not genai:
+        exit()
+    
+    print("available models")
+    for m in genai.list_models():
+        if 'generateContent' in m.supported_generation_methods:
+            print(f"{m.name}")
+    
+    while True:
+        user_input=input("am here to help,Ask me anything...   (Q) to exit \n")
+        if user_input.lower()=='quit':
+            break
+        simple_chat(user_input)
---
+++ README.md
@@ -0,0 +1,4 @@
+## Basic API call to Google's Gemini model
+
+- This project is about prompt Engineering  and in these phase we will try to test the api.
+- 
